2406eca17917747cd56471170b80e013a65b81bd
Nicolas <nicolasbackprogrammer@gmail.com>
2026-06-08 10:52:13 -0300

    :heavy_plus_sign: build: Criando os Controllers de Comments com Get, GetById e Post

---
+++ api/Controllers/CommentsController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Comment;
+using api.Interfaces;
+using api.Mappers;
+using api.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        private readonly IStockRepository _stockRepo;
+        public CommentsController(ICommentRepository commentRepository, IStockRepository stockRepo)
+        {
+            _commentRepo = commentRepository; 
+            _stockRepo = stockRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+
+            var commentDto = comments.Select(s => s.ToCommentDto());
+            
+            return Ok(commentDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID([FromRoute] int id)
+        {
+            var comment = await _commentRepo.GetByIdAsync(id);
+
+            if(comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, [FromBody] CreateCommentDto commentDto)
+        {
+            if(!await _stockRepo.StockExists(stockId))
+            {
+                return BadRequest("Stock does not exists");
+            }
+
+            var commentModel = commentDto.ToCommentFromCreate(stockId);
+            await _commentRepo.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetByID), new {id = commentModel}, commentModel.ToCommentDto());
+        }
+    }
+}
