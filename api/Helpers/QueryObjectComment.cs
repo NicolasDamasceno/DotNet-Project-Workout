c2bcdca9626afe425132a393a644eb7161d3bdea
Nicolas <nicolasbackprogrammer@gmail.com>
2026-06-09 11:20:15 -0300

    :heavy_plus_sign: build: Criando QueryObject para auxiliar na  paginação e filtro de Comment em Repository

---
+++ api/Helpers/QueryObjectComment.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Helpers
+{
+    public class QueryObjectComment
+    {
+        public string? Title { get; set; } = null;
+
+        public bool IsDecsending { get; set; } = false;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 20;
+
+    }
+}
