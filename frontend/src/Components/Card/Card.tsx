16cd14999dd5c6e7c8b1940552b9438cd484e969
Nicolas <nicolasbackprogrammer@gmail.com>
2026-06-16 08:35:27 -0300

    :sparkles: feat: Abstraindo os cards para gerar Cards com valores dinamicos da pesquisa

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,26 +1,33 @@
-import React, { JSX } from 'react'
+import React, { JSX, SyntheticEvent } from 'react'
 import "./Card.css";
+import { CompanySearch } from '../../company';
+import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
 
 interface Props {
-  companyName: string;
-  ticker: string;
-  price: number;
+  id: string;
+  searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props>= ({ companyName , ticker, price }: Props) : JSX.Element => {
+const Card: React.FC<Props>= ({id, searchResult, onPortfolioCreate }: Props) : JSX.Element => {
   return (
     <div className = "card">
-        <img src="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"
-        alt="dogImage"/>
+        <img
+        alt="companyLogo"/>
 
         <div className="details">
-            <h2>{companyName} - {ticker}</h2>
-            <p>{price}</p>
+          <h2>
+            {searchResult.name} - {searchResult.symbol}
+          </h2>
+          <p>{searchResult.currency}</p>
         </div>
         <p className="info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque alias nobis inventore, aspernatur maxime deleniti, iusto temporibus id delectus, quia blanditiis illo accusamus vel aliquid ipsum eum necessitatibus doloremque quisquam.
+          {searchResult.exchangeShortName} - {searchResult.stockExchange}
         </p>
+        <AddPortfolio 
+          onPortfolioCreate={onPortfolioCreate} 
+          symbol={searchResult.symbol}/>
     </div>
   );
-}
+};
 export default Card
