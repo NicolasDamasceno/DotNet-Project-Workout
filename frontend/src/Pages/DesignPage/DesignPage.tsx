b904911a591e9fac9372b3a1bf9191dbd0e06e79
Nicolas <nicolasbackprogrammer@gmail.com>
2026-06-22 13:50:57 -0300

    :sparkles: feat: Adicionado table de IncomeStatement e tableConfig

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import Table from '../../Components/Table/Table';
 import RadioList from '../../Components/RadioList/RadioList';
+import { CompanyKeyMetrics } from '../../company';
+import { testIncomeStatementData } from '../../Components/Table/testData';
 
-type Props = {}
+type Props = {};
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+]
 
 const DesignPage = (props: Props) => {
   return (
     <>
         <h1>FinShark Design Page</h1>
-        <RadioList />
+        <RadioList data={testIncomeStatementData} config={tableConfig}/>
         <h2>This is FinShar's design page. 
             This is were e well house varios design aspects of the app.</h2>
-        <Table />
+        <Table data={testIncomeStatementData} config={tableConfig} />
     </>
   )
 }
